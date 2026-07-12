798168cb83a8bd403ae9ad640d4f87d1813aa742
Gokben <49718141+Gokben@users.noreply.github.com>
2026-07-12 15:00:32 +0300

    Restore Sofitel login background

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ $profileStmt=db()->prepare('SELECT setting_value FROM settings WHERE setting_key
 <link rel="stylesheet" href="<?=url('assets/amerce/fonts/fonts.css')?>"><link rel="stylesheet" href="<?=url('assets/amerce/icon/icomoon/style.css')?>">
 <link rel="stylesheet" href="<?=url('assets/amerce/css/bootstrap.min.css')?>"><link rel="stylesheet" href="<?=url('assets/amerce/css/styles.css')?>">
 <link rel="stylesheet" href="<?=url('assets/amerce-lf.css')?>"><link rel="stylesheet" href="<?=url('assets/vuexy-inspired.css')?>"><link rel="stylesheet" href="<?=url('assets/dashboard-header.css')?>">
-<link rel="stylesheet" href="<?=url('assets/green-buttons.css?v=20260712-2')?>"><script src="<?=url('assets/theme.js?v=20260712-2')?>"></script></head>
+<link rel="stylesheet" href="<?=url('assets/green-buttons.css?v=20260712-3')?>"><script src="<?=url('assets/theme.js?v=20260712-2')?>"></script></head>
 <body>
 <header class="app-header">
   <div class="header-top">
